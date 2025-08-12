1be9263e33fedf6a46856b02185b536697a00b4b
Ashish Anand <aashishanand07@gmail.com>
2025-08-12 19:04:23 +0530

    db updates

---
+++ database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const dbConfig = {
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+}
+const connectDB = async () => {
+  await mongoose.connect(`mongodb+srv://${dbConfig.user}:${dbConfig.password}@cluster0.ymgrbgi.mongodb.net/${dbConfig.database}?retryWrites=true&w=majority`);
+};
+
+module.exports = connectDB;
