9699b8282b3f92c6253f6842af0341679788bae3
Ashishh24 <aashishanand07@gmail.com>
2025-08-19 15:50:27 +0530

    app changed

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const http = require("http");
 const express = require("express");
-const connectDB = require("../database");
+const connectDB = require("./database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const { initializeSocket } = require("./utils/socket")
+const { initializeSocket } = require("./src/utils/socket")
 
 const app = express();
-const User = require("./models/schema");
+const User = require("./src/models/schema");
 
 app.use(cors({
     origin:"*",
@@ -16,11 +16,11 @@ app.use(cors({
 app.use(express.json()); // a middleware that converts all json to js object
 app.use(cookieParser()); // Get cookie from web
 
-const authRouter = require("./routesContainer/auth");
-const profileRouter = require("./routesContainer/profile");
-const requestRouter = require("./routesContainer/request");
-const userRouter = require("./routesContainer/user");
-const uploadRouter = require("./routesContainer/upload");
+const authRouter = require("./src/routesContainer/auth");
+const profileRouter = require("./src/routesContainer/profile");
+const requestRouter = require("./src/routesContainer/request");
+const userRouter = require("./src/routesContainer/user");
+const uploadRouter = require("./src/routesContainer/upload");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
